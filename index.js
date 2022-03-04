4499bdf5f019b1f43df2ae8097519cbef4e777ca
ansh-the-developer <93419521+ansh-the-developer@users.noreply.github.com>
2022-03-04 17:53:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+
+const characters=[{
+    img:'Images/chan.jpg',
+    name:'Sinchan Nohara',
+    title:'Coolest Kid in The Block',
+    description:'Loves: Action Kamen'
+},
+{
+    img:'Images/doremon.png',
+    name:'Doremon',
+    title:'Robo??? Cat??? ROBOcat!!!',
+    description:'Loves: Doracake'
+},
+{
+    img:'Images/jerry.jpg',
+    name:'Jerry',
+    title:'Does all the mischives .',
+    description:'Loves: Cheese '
+},
+{
+    img:'Images/tom.png',
+    name:'Tom',
+    title:'The supposedly evil one',
+    description:'Loves:to sleep'
+},
+]
+
+
+var currentSlideIndex=0;
+
+function displaySlide() {
+    var file = characters[ currentSlideIndex ].img;
+
+    document.getElementById("photo").src = file;
+    document.getElementById("names").innerHTML=characters[ currentSlideIndex ].name; 
+    document.getElementById("titles").innerHTML=characters[ currentSlideIndex ].title;
+    document.getElementById("descriptions").innerHTML=characters[ currentSlideIndex ].description;
+}
+
+function nextSlide() {
+    currentSlideIndex++;
+    if( currentSlideIndex >= characters.length ) {
+        currentSlideIndex = 0;
+    }
+    displaySlide();
+}
+// function previousSlide() {
+//     currentSlideIndex++;
+//     if( currentSlideIndex >= characters.length ) {
+//         currentSlideIndex =characters[characters.length] ;
+//     }
+    
+//     displaySlide();
+// }
+
+function goRandom() {
+    window.alert('                                  ***WARNING*** \n this button will generate random name and descrition ')
+    document.getElementById("photo").src=characters[Math.floor(Math.random()*4)].img;
+    document.getElementById("names").innerHTML=characters[[Math.floor(Math.random()*4)]].name; 
+    document.getElementById("titles").innerHTML=characters[[Math.floor(Math.random()*4)]].title;
+    document.getElementById("descriptions").innerHTML=characters[[Math.floor(Math.random()*4)]].description;
+    
+}
+
+
+// function backButton() {
+
+    
+     
+   
+//     document.getElementById("photo").src=characters[2].img;
+//     document.getElementById("names").innerHTML=characters[2].name; 
+//     document.getElementById("titles").innerHTML=characters[2].title;
+//     document.getElementById("descriptions").innerHTML=characters[2].description;
+    
+   
+    
+// }
+    
+// function forwardButton() {
+//     document.getElementById("photo").src=characters[3].img;
+//     document.getElementById("names").innerHTML=characters[3].name; 
+//     document.getElementById("titles").innerHTML=characters[3].title;
+//     document.getElementById("descriptions").innerHTML=characters[3].description;
+//     // i++;
+// }
+
